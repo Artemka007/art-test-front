4aa464e46d3f1da63e8897aab96cc130074e7bb9
Artem Kopylov <artem.kopylov@aimestech.com>
2025-10-22 19:19:52 +0500

    build(update)

--- src/app/components/company-detail/company-detail.module.ts
+++ src/app/components/company-detail/company-detail.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CompanyDetailRoutingModule } from './company-detail-routing.module';
-
 @NgModule({
   declarations: [],
-  imports: [CommonModule, CompanyDetailRoutingModule],
+  imports: [CommonModule],
 })
 export class CompanyDetailModule {}
