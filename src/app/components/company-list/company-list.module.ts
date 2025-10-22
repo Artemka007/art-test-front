df9d04428bf381f5082258a0d37d1e8261b5329b
Artem Kopylov <artem.kopylov@aimestech.com>
2025-10-22 15:19:14 +0300

    feature(components): init components for routers

--- src/app/components/company-list/company-list.module.ts
+++ src/app/components/company-list/company-list.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CompanyItemComponent } from './company-item/company-item.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CompanyItemComponent
+  ],
   imports: [
     CommonModule
   ]
---
+++ src/app/types/company.type.ts
@@ -0,0 +1,14 @@
+export type Company = {
+  id: 42;
+  uid: string;
+  business_name: string;
+  suffix: string;
+  industry: string;
+  type: string;
+  catch_phrase: string;
+  phone_number: string;
+  full_address: string;
+  latitude: number;
+  longitude: number;
+  logo: string;
+}
